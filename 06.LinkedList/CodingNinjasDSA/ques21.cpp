a90a38a78ed764f7c4b11716647590fc9ba8a05b
Ranjeet <sranjeet434@gmail.com>
2023-04-28 14:00:40 +0530

    prep

--- 06.LinkedList/CodingNinjasDSA/ques21.cpp
+++ 06.LinkedList/CodingNinjasDSA/ques21.cpp
@@ -1,41 +1,55 @@
-// // Bubble Sort (Iterative) LinkedList
-// #include<bits/stdc++.h>
-// using namespace std;
-// class Node{
-//     public:
-//     int data;
-//     Node *next;
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
 
-//     Node(int data){
-//         this->data=data;
-//         this->next=NULL;
-//     }
-// };
-// int lenght(Node *head){
-//     if(head==NULL){
-//         return 0;
-//     }
-//     return 1+lenght(head->next);
-// }
-// Node *bubbleSort(Node *head){
-//     int n=lenght(head->next);
-//     for(int i=0;i<n;i++){
-//         Node *prev=NULL;
-//         Node *curr=head;
-//         Node *nxt=NULL;
-//         for(int j=0;j<n-1-i;j++){
-//             if(curr->data > curr->next->data){
-//                 if(prev==NULL){
-//                     nxt=curr->next;
-//                     head=nxt;
-//                     curr->next=nxt->next;
-//                     nxt->next=curr;
-//                     prev=nxt;
-//                 }else{
-
-//                 }
-//             }
-//         }
-
-//     }
-// }
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+class Pair{
+    public:
+    Node *head;
+    Node *tail;
+};
+Pair reverseLL(Node *head){
+    if(head==NULL ||head->next==NULL){
+        Pair ans;
+        ans.head=head;
+        ans.tail=head;
+        return ans;
+    }
+    Pair samllans=reverseLL(head->next);
+    smallans.tail->next=head;
+    head->next=NULL;
+    Pair ans;
+    ans.head=smallans.head;
+    ans.tail=head;
+    return ans;
+}
+Node *kRverse(Node *head,int k){
+    if(head==NULL){
+        return head;
+    }
+    if(k==0 || k==1){
+        return head;
+    }
+    Node *temp=head;
+    int count=0;
+    while(temp!=NULL || count!=k-1){
+        temp=temp->next;
+        count++;
+    }
+    Node *head2=NULL;
+    if(temp!=NULL){
+        head2=temp->next;
+        temp->next=NULL;
+    }
+    Node *newHead=kRverse(head2,k);
+    Pair ans=reverseLL(head);
+    ans.tail->next=newHead;
+    return ans.head;
+}
