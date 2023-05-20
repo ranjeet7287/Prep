137217f6ae616ef282c039002dfbc80e8a7cce06
Ranjeet <sranjeet434@gmail.com>
2023-05-20 12:31:56 +0530

    prep

--- 12.DP/1.KnapsackProblem/ques5.cpp
+++ 12.DP/1.KnapsackProblem/ques5.cpp
@@ -2,7 +2,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int SubsetSum(int *arr,int n,int range){
-    int t[n+1][range+1];
-    
+
+int SubsetSum(int *arr,int n,int sum){
+    int  t[n+1][sum+1];
+    // Intailizeing 
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<sum+1;j++){
+            if(i==0){
+                t[i][j]=0;
+            }
+            if(j==0){
+                t[i][j]=1;
+            }
+        }
+    }    
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<sum+1;j++){
+            if(arr[i-1]<=j){
+                t[i][j]=t[i-1][j-arr[i-1]] || t[i-1][j];
+            }else{
+                t[i][j]=t[i-1][j];
+            }
+        }
+    }
+
+    int ans = INT_MAX;
+    //All the subset sum which is possible for this n sized array will have true value in table in nth row
+    // Iterating last row where the sum exists
+    // abs(sum - 2*(subset_sum) will give subset sum difference
+    for(int j = 0; j<sum+1; j++){
+        if(t[n][j] == 1) ans = min(ans, abs(sum - 2*j));
+    }
+    return ans;
+}
+
+
+
+int MinDiff(int *arr,int n){
+    int sum=0;
+    for(int i=0;i<n+1;i++){
+        sum+=arr[i];
+    }
+    return SubsetSum(arr,n,sum);
 }
---
+++ 12.DP/1.KnapsackProblem/ques7.cpp
@@ -0,0 +1,45 @@
+// Target Sum
+// Same as  Count the number of subset with a given difference
+// just here Sum  --- > equal to diff
+#include<bits/stdc++.h>
+using namespace std;
+
+int CountSubset(int *arr,int n,int sum){
+    int t[n+1][sum+1];
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<sum+1;j++){
+            if(i==0){
+                t[i][j]=0;
+            }
+            if(j==0){
+                t[i][j]=1;
+            }
+        }
+    }    
+    if(arr[n-1]<=sum){
+        t[n][sum]=t[n-1][sum-arr[n-1]] + t[n-1][sum];
+    }else{
+        t[n][sum]=t[n-1][sum];
+    }
+    
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<sum+1;j++){
+            if(arr[i-1]<=j){
+                t[i][j]=t[i-1][j-arr[i-1]] + t[i-1][j];
+            }else{
+                t[i][j]=t[i-1][j];
+            }
+        }
+    }
+    return t[n][sum];
+}
+
+
+int CountSubDiff(int *arr,int n,int diff){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    int s1=diff+sum/2;
+    return CountSubset(arr,n,s1);
+}
