83aee436ac08e0f7aee9c97856a297a5bf32cd11
Ranjeet <sranjeet434@gmail.com>
2023-05-20 21:31:12 +0530

    prep

---
+++ 12.DP/2.UnboundedKnapsack/ques3.cpp
@@ -0,0 +1,29 @@
+// Coin Change Problem (Maximum number of ways)
+#include<bits/stdc++.h>
+using namespace std;
+
+int CoinChange(int *coin,int n,int sum){
+
+    int t[n+1][sum+1];
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<sum+1;j++){
+            if(j==0){
+                t[i][j]=1;
+            }
+            if(i==0){
+                t[i][j]=0;
+            }
+        }
+    }
+
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<sum+1;j++){
+            if(coin[i-1]<=j){
+                t[i][j]=t[i][j-coin[i-1]]+t[i-1][j];
+            }else{
+                t[i][j]=t[i-1][j];
+            }
+        }
+    }
+    return t[n][sum];
+}
